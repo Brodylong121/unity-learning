9264dd4732cb07a32d0b93231cc4e3ccd74c294f
Diogo Muller <diogo.muller@live.com>
2013-10-09 10:32:55 -0300

    LAB01 - Finished optional objectives.

---
+++ Lab 1 - Point and Click/Assets/Scripts/Actors/CircularMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircularMovement : MonoBehaviour 
+{
+    #region Inspector Variables
+    /// <summary>
+    /// Object speed.
+    /// </summary>
+    public float speed = 0.2f;
+    public float rotation = 10f;
+    #endregion Inspector Variables
+
+    #region Private Variables
+
+    #endregion Private Variables
+
+    #region Game Cycle Methods
+    /// <summary>
+    /// Initializes the object.
+    /// </summary>
+    void Start()
+    {
+    }
+
+    /// <summary>
+    /// Update is called every frame.
+    /// </summary>
+    void Update()
+    {
+        transform.Rotate(0, 0 , rotation);
+        transform.Translate(speed, 0 , 0);
+    }
+    #endregion Game Cycle Methods
+
+    #region Methods
+    #endregion Methods
+}
