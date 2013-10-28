6507ea8e11a870ced548c940bffe6d9a45888a2d
Diogo Muller <diogo.muller@live.com>
2013-10-28 14:42:26 -0200

    LAB03: Timer Tools - Class setup.

---
+++ Lab 3 - Tool Development/Assets/Scripts/Tools/PrintableTimer.cs
@@ -0,0 +1,170 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrintableTimer : Timer 
+{
+	#region Inspector Variables
+	public GameObject animationFont01;
+	public GameObject animationFont02;
+	public GameObject animationFont03;
+	public GameObject animationFont04;
+	
+	public GUISkin marioGui;
+	#endregion Inspector Variables
+	
+	#region Game Cycle
+	/// <summary>
+	/// Use this for initialization
+	/// </summary>
+	void Start () 
+	{
+	
+	}
+	
+	/// <summary>
+	/// Update is called once per frame.
+	/// </summary>
+	public override void Update () 
+	{
+		AnimateSprite( animationFont01, 10, 1, 0, 0, 10, "font1");
+		AnimateSprite( animationFont02, 10, 1, 0, 0, 10, "font2");
+		AnimateSprite( animationFont03, 10, 1, 0, 0, 10, "font3");
+		AnimateSprite( animationFont04, 10, 1, 0, 0, 10, "font4");
+		
+		base.Update();		
+		
+		// Start Time.		
+		if( Input.GetKeyDown(KeyCode.Alpha1) )
+		{
+			StartTimer();
+		}
+		
+		// From Load Time.
+		if( Input.GetKeyDown(KeyCode.Alpha2) )
+		{
+			FromLoad();
+		}
+		
+		// Stops time.
+		if( Input.GetKeyDown(KeyCode.Alpha3) )
+		{
+			StopTimer();
+		}
+		
+		// Time scale.
+		if( Input.GetKeyDown(KeyCode.Alpha4) )
+		{
+			ChangeScale(0f);
+		}
+		else if( Input.GetKeyUp(KeyCode.Alpha4) )
+		{
+			ChangeScale(1f);
+		}
+		
+		// Continue time.
+		if( Input.GetKeyDown(KeyCode.Alpha5) )
+		{
+			Resume();
+		}
+		
+		// Reset time.
+		if( Input.GetKeyDown(KeyCode.Alpha6) )
+		{
+			Reset();
+		}
+		
+		// Countdown
+		if( Input.GetKeyDown(KeyCode.Alpha7) )
+		{
+			Countdown(5);
+		}
+
+		// Adds to timer a single amount, once.
+		if( Input.GetKeyDown(KeyCode.Alpha8) )
+		{
+			SetExtraTime(1f);
+		}
+		
+		// Adds to timer every time.
+		if( Input.GetKeyDown(KeyCode.Alpha9) )
+		{
+			AddToTimer(1f);
+		}
+		
+		// Gets real time.
+		if( Input.GetKeyDown(KeyCode.Alpha0) )
+		{
+			RealTime();
+		}
+	}
+	
+	void OnGUI()
+	{		
+		GUILayout.Label("PlayTime " + playTime.ToString("f4") );
+		GUILayout.Label("1 - Start the Time");
+		GUILayout.Label("2 - From Load Time");
+		GUILayout.Label("3 - Stop Time");
+		GUILayout.Label("4 - Pause Game Time");
+		GUILayout.Label("5 - Continue Time");
+		GUILayout.Label("6 - Reset Time");
+		GUILayout.Label("7 - Count Down Time");
+		GUILayout.Label("8 - Add to Time Once");
+		GUILayout.Label("9 - Add to Time Multi");
+		GUILayout.Label("0 - Time Since Startup");
+		
+		GUILayout.Label("Minutes:     " + minutes.ToString("f0") );
+		GUILayout.Label("Seconds:     " + seconds.ToString("f0") );
+		GUILayout.Label("Miliseconds: " + fractions.ToString("f0") );
+		
+		GUILayout.Label("Delay Time " + delayTime.ToString("f4") );
+		GUILayout.Label("Continue Time " + continueTime.ToString("f4") );
+		
+		GUI.skin = marioGui;
+		
+		GUI.Label( new Rect( Screen.width / 2, 10, 1000, 100), "" + playTime.ToString("f1"));
+		
+	}
+	#endregion Game Cycle
+	
+	#region Methods
+	/// <summary>
+	/// Animates the sprite.
+	/// </summary>
+	/// <param name='columnSize'>Column size.</param>
+	/// <param name='rowSize'>Row size.</param>
+	/// <param name='columnFrameStart'>Column frame start.</param>
+	/// <param name='rowFrameStart'>Row frame start.</param>
+	/// <param name='totalFrames'>Total frames.</param>
+	/// <param name='framesPerSecond'>Frames per second.</param>
+	public void AnimateSprite( GameObject spriteObject, int columnSize, int rowSize, int columnFrameStart, int rowFrameStart, int totalFrames, string type)
+	{
+		// Modulate
+		int index = Mathf.CeilToInt(playTime);
+		
+		int font1 = index % 10;
+		int font2 = ( (index - font1 ) / 10 ) % 10;
+		int font3 = ( (index - font1 ) / 100 ) % 10;
+		int font4 = ( (index - font1 ) / 1000 ) % 10;
+		
+		if( type == "font1" ) index = font1;
+		if( type == "font2" ) index = font2;
+		if( type == "font3" ) index = font3;
+		if( type == "font4" ) index = font4;
+		
+		// Transforms index in current column and row.
+		int u = index % columnSize;
+		int v = index / columnSize;
+		
+		// Calculates size and offset.
+		Vector2 size = new Vector2( 1.0f / columnSize, 1.0f / rowSize );
+		Vector2 offset = new Vector2( ( u + columnFrameStart ) * size.x, (1 - size.y) - ( (v + rowFrameStart) * size.y) );
+		
+		// Sets values on the texture.
+		spriteObject.renderer.material.mainTextureOffset = offset;
+		spriteObject.renderer.material.mainTextureScale = size;
+		
+		//renderer.material.SetTextureOffset("_BumpMap", offset);
+		//renderer.material.SetTextureScale("_BumpMap", size);
+	}
+	#endregion Methods
+}
--- Lab 3 - Tool Development/Assets/Scripts/Tools/Timer.cs
+++ Lab 3 - Tool Development/Assets/Scripts/Tools/Timer.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/// <summary>
+/// Timer type.
+/// </summary>
 public enum TimerType
 {
 	Playtime,
@@ -13,32 +16,82 @@ public enum TimerType
 public class Timer : MonoBehaviour
 {
 	#region Inspector Variables
+	/// <summary>
+	/// The play time.
+	/// </summary>
 	public float playTime = 0f;	
+	/// <summary>
+	/// The days.
+	/// </summary>
 	public float days = 0f;
+	/// <summary>
+	/// The hours.
+	/// </summary>
 	public float hours = 0f;
+	/// <summary>
+	/// The minutes.
+	/// </summary>
 	public float minutes = 0f;
+	/// <summary>
+	/// The seconds.
+	/// </summary>
 	public float seconds = 0f;
+	/// <summary>
+	/// The fractions.
+	/// </summary>
 	public float fractions = 0f;
+	/// <summary>
+	/// The delay rate.
+	/// </summary>
+	public float delayRate = 0f;
 	#endregion Inspector Variables
 	
-	#region Private Variables
-	private float startTime = 0f;
-	private float fromLoadTime = 0f;
-	private float stopTime = 0f;
-	private float continueTime = 0f;
-	private float countdownDelay = 0f;
-	private float countdownAmount = 0f;
-	private float delayTime = 0f;
-	private float delayRate = 0f;
-	private float addToTime = 0f;
-	private float addTimeAmount = 0f;
-	private float realTime = 0f;
+	#region protected Variables
+	/// <summary>
+	/// The start time.
+	/// </summary>
+	protected float startTime = 0f;
+	/// <summary>
+	/// From load time.
+	/// </summary>
+	protected float fromLoadTime = 0f;
+	/// <summary>
+	/// The stop time.
+	/// </summary>
+	protected float stopTime = 0f;
+	/// <summary>
+	/// The continue time.
+	/// </summary>
+	protected float continueTime = 0f;
+	/// <summary>
+	/// The countdown delay.
+	/// </summary>
+	protected float countdownDelay = 0f;
+	/// <summary>
+	/// The countdown amount.
+	/// </summary>
+	protected float countdownAmount = 0f;
+	/// <summary>
+	/// The delay time.
+	/// </summary>
+	protected float delayTime = 0f;
+	/// <summary>
+	/// Extra time added to time.
+	/// </summary>
+	protected float addToTime = 0f;
+	/// <summary>
+	/// The real time.
+	/// </summary>
+	protected float realTime = 0f;
 	
-	private TimerType currentTimer = TimerType.Inactive;
-	#endregion Private Variables
+	protected TimerType currentTimer = TimerType.Inactive;
+	#endregion protected Variables
 	
 	#region Cycle Methods
-	void Update()
+	/// <summary>
+	/// Update this instance.
+	/// </summary>
+	public virtual void Update()
 	{
 		days = ( playTime / 86400);
 		hours = (playTime / 3600) % 24;
@@ -48,7 +101,7 @@ public class Timer : MonoBehaviour
 		
 		switch( currentTimer )
 		{
-			case playTime:
+			case TimerType.Playtime:
 				playTime = Time.time - startTime - continueTime + addToTime;
 				break;
 			case TimerType.FromLoad:
@@ -56,13 +109,156 @@ public class Timer : MonoBehaviour
 				break;
 			case TimerType.Countdown:
 				playTime = countdownDelay - Time.time + countdownAmount;
+			
+				if( playTime < 0 )
+				{
+					currentTimer = TimerType.Inactive;
+				}
 				break;
 			case TimerType.Realtime:
 				playTime = Time.realtimeSinceStartup + addToTime;
 				break;
 			default:
-				playTime = 0f;
+				break;
+		}
+		
+		if( playTime > delayTime )
+		{
+			delayTime = playTime + delayRate;
 		}
 	}
 	#endregion Cycle Methods
+	
+	#region Public Methods
+	/// <summary>
+	/// Starts the time as Play Time..
+	/// </summary>
+	public void StartTimer()
+	{
+		startTime = Time.time;
+		addToTime = 0f;
+		continueTime = 0f;
+		currentTimer = TimerType.Playtime;
+	}
+	
+	/// <summary>
+	/// Starts the time as the load time.
+	/// </summary>
+	public void FromLoad()
+	{
+			fromLoadTime = Time.timeSinceLevelLoad;
+			startTime = 0f;
+			addToTime = 0f;
+			currentTimer = TimerType.FromLoad;
+	}
+	
+	/// <summary>
+	/// Stops the timer.
+	/// </summary>
+	public void StopTimer()
+	{
+		stopTime = playTime;
+		addToTime = 0;
+		currentTimer = TimerType.Inactive;
+	}
+	
+	/// <summary>
+	/// Changes the time scale.
+	/// </summary>
+	/// <param name='scale'>New time scale.</param>
+	public void ChangeScale(float scale)
+	{
+		Time.timeScale = scale;
+	}
+	
+	/// <summary>
+	/// Resumes the timer.
+	/// </summary>
+	public void Resume()
+	{
+		continueTime = Time.time - playTime;
+		startTime = 0f;
+		addToTime = 0f;
+		currentTimer = TimerType.Playtime;
+	}
+	
+	/// <summary>
+	/// Reset the timer.
+	/// </summary>
+	public void Reset()
+	{
+		stopTime = 0f;
+		playTime = 0f;
+		continueTime = 0f;
+		addToTime = 0f;
+		currentTimer = TimerType.Inactive;
+	}
+	
+	/// <summary>
+	/// Countdown the specified time.
+	/// </summary>
+	/// <param name='time'>Time for countdown.</param>
+	public void Countdown(float time)
+	{
+		countdownDelay = Time.time;
+		countdownAmount = time;
+		addToTime = 0;
+	}
+	
+	/// <summary>
+	/// Adds to timer.
+	/// </summary>
+	/// <param name='time'>Time.</param>
+	public void AddToTimer(float time)
+	{
+		addToTime += time;
+	}
+	
+	/// <summary>
+	/// Sets the extra time.
+	/// </summary>
+	/// <param name='time'>Time.</param>
+	public void SetExtraTime(float time)
+	{
+		addToTime = time;
+	}
+
+	/// <summary>
+	/// Starts the counter using realtime.
+	/// </summary>
+	public void RealTime()
+	{
+		realTime = Time.realtimeSinceStartup;
+		startTime = 0f;
+		addToTime = 0f;
+		currentTimer = TimerType.Realtime;
+	}
+	
+	/// <summary>
+	/// Changes the timer to the specified type.
+	/// </summary>
+	/// <param name='type'>New type.</param>
+	/// <param name='countdownTime'>Countdown time for countdown timer.</param>
+	public void ChangeTimer(TimerType type, float countdownTime = 0f)
+	{
+		switch(type)
+		{
+			case TimerType.Playtime:
+				StartTimer();
+				break;
+			case TimerType.FromLoad:
+				FromLoad();
+				break;
+			case TimerType.Countdown:
+				Countdown(countdownTime);
+				break;
+			case TimerType.Realtime:
+				RealTime();
+				break;
+			case TimerType.Inactive:
+				StopTimer();
+				break;			
+		}
+	}
+	#endregion Public Methods
 }
